48f8189e547ab7d1106cc8e79c9fcf36a125c227
X09010LO <miguelangel.lopezcastilla@viajeseci.es>
2020-03-18 09:26:33 +0100

    Quitadas las configuraciones que no sean sólo de Spring

--- test-project/src/main/java/com/ecofoo/test/Application.java
+++ test-project/src/main/java/com/ecofoo/test/Application.java
@@ -1,15 +1,18 @@
 package com.ecofoo.test;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableConfigurationProperties
 @SpringBootApplication
-@ComponentScan(basePackages = { "org.openapitools.configuration" })
 public class Application {
+    private static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+
+        context = builder.run(args);
     }
 }
