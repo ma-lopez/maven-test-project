7d0099cf0fce7315e9a3a223534a1ca22ad4881c
X09010LO <miguelangel.lopezcastilla@viajeseci.es>
2020-03-19 08:18:03 +0100

    - Añadido lombok - Quitada la configuración del maven del git.

--- test-project/src/main/java/com/ecofoo/test/Application.java
+++ test-project/src/main/java/com/ecofoo/test/Application.java
@@ -1,5 +1,6 @@
 package com.ecofoo.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableConfigurationProperties
 @SpringBootApplication
+@Slf4j
 public class Application {
     private static ConfigurableApplicationContext context;
 
@@ -14,5 +16,7 @@ public class Application {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
 
         context = builder.run(args);
+
+        log.info("------ [Application {} running...] ------", context.getApplicationName());
     }
 }
